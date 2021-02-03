09540fa03f2d481d7c6a8fb1af0b5a311e716415
Kimbangg <ehehdgus1@hanyang.ac.kr>
2021-02-03 12:44:52 +0900

    Merge Complete

--- src/App.js
+++ src/App.js
@@ -1,22 +1,31 @@
 import './App.css';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
 // Header & Footer
 import Header from './components/views/Section/Header/Header';
 import Footer from './components/views/Section/Footer/Footer';
+
 // Work Page
 import IdusPage from './components/views/IdusPage/IdusPage';
 import WorkPage from './components/views/WorkPage/WorkPage';
 import WorkCategoryPage from './components/views/WorkPage/WorkCategoryPage';
+import WorkDetailPage from './components/views/WorkPage/WorkDetailPage';
+
 // Class Page
 import ClassPage from './components/views/ClassPage/ClassPage';
 import Popular from './components/views/ClassPage/PopularClass';
 import MyPage from './components/views/MyPage/MyPage';
+
 // 로그인 및 회원정보 Directory
 import Login from './components/views/LoginPage/LoginPage';
 import LoginPage from './components/views/LoginPage/LoginPage';
 import SignUp from './components/views/LoginPage/SIgnUpStage';
 import Personal from './components/views/LoginPage/Personal';
 import DeliveryAdd from './components/views/LoginPage/DeliveryAdd';
+import MyInfo from './components/views/myinfo/myinfo';
+// Cart 관련 category
+import Cart_W from './components/views/CartPage/CartPage_W';
+import Pay_W from './components/views/CartPage/Pay_W';
 
 function App() {
   return (
@@ -27,9 +36,13 @@ function App() {
           <Route exact path="/main" component={IdusPage} />
           <Route exact path="/main/work" component={WorkPage} />
           <Route exact path="/main/work/category/:id" component={WorkCategoryPage} />
+          <Route exact path="/work/detail/:id" component={WorkDetailPage} />
           <Route exact path="/main/class" component={ClassPage} />
           <Route exact path="/main/login" component={LoginPage} />
           <Route exact path="/main/mypage" component={MyPage} />
+          <Route exact path="/w/cart/:id" component={Cart_W} />
+          <Route exact path="/main/myInfo" component={MyInfo} />
+          <Route exact path="/w/pay/:id" component={Pay_W} />
           <Route exact path="/main/class/popular" component={Popular} />
           <Route exact path="/user/join" component={Login} />
           <Route exact path="/user/join/signup" component={SignUp} />
--- src/components/views/Section/Header/Header.js
+++ src/components/views/Section/Header/Header.js
@@ -1,7 +1,110 @@
 import React from 'react';
+import '../../../css/Header.css';
+
+import idusimage from '../../../img/idus.png';
+import { BsPerson } from 'react-icons/bs';
+import { FiShoppingCart } from 'react-icons/fi';
+//import Jumbotron from 'react-bootstrap/Jumbotron'
+//import {BootstrapTable, TableHeaderColumn} from 'react-bootstrap-table';
+//css 디자인에 앞써 npm install --save styled-components 설치 css적용을 위해
+//import Icon from './components/icons' icon 불러오려고
+/*npm install react-icons --save 설치하기 icon 표시하기 위해*/
+//npm install react-bootstrap-table --save 설치하기 테이블 생성
+// import Modal from 'react-native-modalbox'  팝업창 띄우기 위한 설치
 
 function Header() {
-  return <div>Header</div>;
+  return (
+    <div>
+      <header>
+        <section className="header">
+          <section className="header-top">
+            <section className="header-top__navbar">
+              <section className="header-top__navigation">
+                <a href="/" className="header-bottom">
+                  로그인
+                </a>
+                <a href="/" className="header-bottom">
+                  회원가입
+                </a>
+                <a href="/" className="header-bottom">
+                  고객센터
+                </a>
+              </section>
+              <hr className="header-top__seperator" />
+            </section>
+          </section>
+          <section className="header-bottom">
+            <section className="header-bottom__info">
+              <BsPerson size="20" color="steelblue" />
+              &nbsp;&nbsp;내정보
+            </section>
+            <section className="header-bottom__shop">
+              <FiShoppingCart size="20" color="steelblue" />
+              &nbsp;&nbsp;장바구니
+            </section>
+          </section>
+        </section>
+
+        <div className="Navbar">
+          <button className="logobutton">
+            <img src={idusimage} />
+          </button>
+
+          <div className="leftSide">
+            <a href="/class" className="linka">
+              클래스
+            </a>
+            <a href="/work" className="linka">
+              작품
+            </a>
+            <div className="links"></div>
+          </div>
+          <div className="rightSide">
+            <input type="text" placeholder="작품/클래스 검색하기" />
+            <button className="btn2">검색</button>
+          </div>
+        </div>
+
+        <div className="Navbar">
+          <div className="links">
+            <a href="/category" className="linkb">
+              카테고리
+            </a>
+            <a href="/home" className="linkb">
+              홈
+            </a>
+            <a href="/today" className="linkb">
+              오늘의발견
+            </a>
+            <a href="/livebuy" className="linkb">
+              실시간 구매
+            </a>
+            <a href="/livereview" className="linkb">
+              실시간 후기
+            </a>
+            <a href="/pick" className="linkb">
+              작가님 추천
+            </a>
+            <a href="/livepick" className="linkb">
+              실시간 추천
+            </a>
+            <a href="/popularwork" className="linkb">
+              인기작품
+            </a>
+            <a href="/story" className="linkb">
+              스토리
+            </a>
+            <a href="/new" className="linkb">
+              최신작품
+            </a>
+            <a href="/popularartist" className="linkb">
+              인기작가
+            </a>
+          </div>
+        </div>
+      </header>
+    </div>
+  );
 }
 
 export default Header;
